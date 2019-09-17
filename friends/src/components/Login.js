1385f75676fca663a352cdca8ed6661d95bda654
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-17 13:35:07 -0600

    axios with auth file set up

---
+++ friends/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Login = () => {
+
+
+    
+    return (
+        <div className="loginWrapper">
+            <form>
+
+            </form>
+        </div>
+    );
+};
