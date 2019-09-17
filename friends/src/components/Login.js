29a23feed728fd5a0c0e803fbd18646ffe7bc553
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-17 14:09:41 -0600

    login almost completed, just missing handleChange

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-export const Login = () => {
-
-
+export const Login = (props) => {
+    const [credentials, setCredentials] = useState({})
+    const login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('/login', credentials) 
+            .then(res => {
+                localStorage.setItem('token', res.data.token);
+                props.history.push('/dashboard');
+            })
+        }
     
+        // const handleChange = e => {
+        //     setCredentials: {
+        //       ...credentials,
+        //       [e.target.name]: e.target.value,
+        //     }
+        // }
+
     return (
         <div className="loginWrapper">
-            <form>
-
+            <form onSubmit={login}>
+                <input 
+                type="text"
+                name="username"
+                placeholder="username"
+                value={credentials.username}
+                // onChange={this.handleChange}
+                />
+                <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={credentials.password}
+                // onChange={handleChange}
+                />
+                <button>Log in</button>
             </form>
         </div>
     );
